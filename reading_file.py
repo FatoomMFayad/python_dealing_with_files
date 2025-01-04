1081c560663237dc0e1ea6b318e7ec65e3c49856
ffayad <ffayad@ptcdb.edu.ps>
2025-01-04 12:23:52 +0200

    read using readLines

--- reading_file.py
+++ reading_file.py
@@ -1,4 +1,10 @@
 #returns file wrapper
 f = open('c:/Users/Fatoom/Documents/names.txt')
-#print the file's content
-print(f.read())
+#print the file's content: read returns string
+# print(f.read())
+#print content of file using readLine
+lines = [i for i in f.readlines()]
+lines2 = []
+for line in lines:
+    lines2.append(line.strip())
+print(lines2)
